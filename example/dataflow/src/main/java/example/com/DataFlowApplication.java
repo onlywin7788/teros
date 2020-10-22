c6dc429029e766b6ef08364aeb69c9ed504d6e8a
batzero <batzerohi@gmail.com>
2020-10-22 14:49:58 +0900

    dataflow 추가

---
+++ example/dataflow/src/main/java/example/com/DataFlowApplication.java
@@ -0,0 +1,14 @@
+package example.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DataFlowApplication.class, args);
+	}
+
+}
