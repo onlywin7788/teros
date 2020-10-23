3fc15b16ca4155397740fb7c63ebb8205bdae731
onlywin7788 <onlywin7788@gmail.com>
2020-10-23 17:40:27 +0900

    commit

---
+++ teros-component/teros-component-spec/teros-component-spec/src/main/java/com/teros/component/spec/TerosComponentSpecApplication.java
@@ -0,0 +1,13 @@
+package com.teros.component.spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerosComponentSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerosComponentSpecApplication.class, args);
+	}
+
+}
---
+++ teros-component/teros-component-spec/teros-component-spec/src/test/java/com/teros/component/spec/TerosComponentSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teros.component.spec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerosComponentSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
